9bce3e8cf19f6636d9d105ae23fa0d671f1f7cba
andrewdavidmackenzie <andrew@mackenzie-serres.net>
2021-01-19 21:05:19 +0100

    WIP glade loading

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,7 @@ mod options;
 mod build_ui;
 mod ui_layout;
 // mod cli_debug_client;//#![deny(missing_docs)]
+mod ui_layout_glade;
 
 // For logging errors related with the UI that suggest displaying them on the UI maybe impossible
 pub fn log_error(message: &str) {
--- src/ui_layout_glade.rs
+++ src/ui_layout_glade.rs
@@ -1,57 +1,18 @@
-#![cfg_attr(not(feature = "gtk_3_10"), allow(unused_variables, unused_mut))]
-
 use gtk::prelude::*;
-use gtk::{gdk, glib};
-use gtk::{
-    AboutDialog, AppChooserDialog, ApplicationWindow, Builder, Button, Dialog, Entry,
-    FileChooserAction, FileChooserDialog, FontChooserDialog, RecentChooserDialog, ResponseType,
-    Scale, SpinButton, Spinner, Switch, Window,
-};
+use gtk::{ApplicationWindow, Builder};
 
-use std::env::args;
-
-// fn about_clicked(button: &Button, dialog: &AboutDialog) {
-//     if let Some(window) = button
-//         .get_toplevel()
-//         .and_then(|w| w.downcast::<Window>().ok())
-//     {
-//         dialog.set_transient_for(Some(&window));
-//     }
-//
-//     // We only want to hide the dialog when it's closed and not completely destroy it
-//     // as otherwise we can't show it again a second time.
-//     dialog.connect_delete_event(|dialog, _| {
-//         dialog.hide();
-//         gtk::Inhibit(true)
-//     });
-//     //
-//     // println!("Authors: {:?}", dialog.get_authors());
-//     // println!("Artists: {:?}", dialog.get_artists());
-//     // println!("Documenters: {:?}", dialog.get_documenters());
-//
-//     // println!(
-//     //     "Major: {}, Minor: {}",
-//     //     gtk::get_major_version(),
-//     //     gtk::get_minor_version()
-//     // );
-//
-//     dialog.show_all();
-// }
+use crate::build_ui::widgets;
 
 pub fn create(application: &gtk::Application) -> widgets::WidgetRefs {
     let glade_src = include_str!("ui.glade");
     let builder = Builder::from_string(glade_src);
 
-    // let spinner: Spinner = builder.get_object("spinner").expect("Couldn't get spinner");
-    // spinner.start();
-
-    let app_window: ApplicationWindow = builder.get_object("window").expect("Couldn't get window");
+    let app_window: ApplicationWindow = builder.get_object("app_window").expect("Couldn't get app_window");
 
     app_window.show_all();
 
     widgets::WidgetRefs {
         app_window,
-        main_window: builder.get_object("main_window").expect("Couldn't get main_window"),
         flow_buffer: builder.get_object("flow_buffer").expect("Couldn't get flow_buffer"),
         manifest_buffer: builder.get_object("manifest_buffer").expect("Couldn't get manifest_buffer"),
         flow_notebook: builder.get_object("flow_notebook").expect("Couldn't get flow_notebook"),
