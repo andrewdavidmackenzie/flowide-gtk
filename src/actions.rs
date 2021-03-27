d003376916167591892028c26cc60509de793ede
Andrew Mackenzie <andrew@mackenzie-serres.net>
2021-03-27 16:07:33 +0100

    Update to latest flow libraries and fix incompatibilities

--- src/actions.rs
+++ src/actions.rs
@@ -7,12 +7,14 @@ use flowclib::generator::generate;
 use flowclib::model::process::Process::FlowProcess;
 use flowrlib::coordinator::{Submission, Coordinator};
 use flowrstructs::manifest::{DEFAULT_MANIFEST_FILENAME, Manifest};
-use provider::content::provider::MetaProvider;
+use provider::content::provider::{MetaProvider, Provider};
 
-use crate::log_error;
+use crate::{log_error, log_warn};
 use crate::build_ui::UICONTEXT;
 use crate::ide_runtime_client::IDERuntimeClient;
 use crate::ui_context::UIContext;
+use std::env;
+use simpath::Simpath;
 
 /// Background action that compiles a flow on a thread and then updates the UI with the resulting
 /// compiled flow manifest
@@ -59,13 +61,39 @@ pub fn compile_flow() {
     });
 }
 
+/*
+    For the lib provider, libraries maybe installed in multiple places in the file system.
+    In order to find the content, a FLOW_LIB_PATH environment variable can be configured with a
+    list of directories in which to look for the library in question.
+
+    Using the "FLOW_LIB_PATH" environment variable attempt to locate the library's root folder
+    in the file system.
+ */
+pub fn get_lib_search_path() -> Simpath {
+    // TODO read additional entries from UI
+    let search_path_additions: &[String] = &[];
+
+    let lib_search_path = Simpath::new_with_separator("FLOW_LIB_PATH", ',');
+
+    if env::var("FLOW_LIB_PATH").is_err() && search_path_additions.is_empty() {
+        log_warn("'FLOW_LIB_PATH' is not set and no LIB_DIRS supplied, so it is possible libraries referenced will not be found");
+    }
+
+    // for addition in search_path_additions {
+    //     lib_search_path.add(addition);
+    //     UIContext::message(&format!("'{}' added to the Library Search Path", addition));
+    // }
+
+    lib_search_path
+}
+
 /// Load a flow from 'url' in a background thread and then update the UI with a JSON representation
 /// of it.
 pub fn open_flow(url: String) {
     std::thread::spawn(move || {
-        let provider = MetaProvider {};
+        let provider = &MetaProvider::new(get_lib_search_path()) as &dyn Provider;
 
-        match loader::load(&url, &provider) {
+        match loader::load(&url, provider) {
             Ok(FlowProcess(flow)) => {
                 match UICONTEXT.try_lock() {
                     Ok(mut context) => context.set_flow(Some(flow)),
@@ -73,7 +101,7 @@ pub fn open_flow(url: String) {
                 }
             }
             Ok(_) => UIContext::ui_error(&format!("Process loaded from Url: '{}' was not of type 'Flow'", url)),
-            _ => UIContext::ui_error(&format!("Could not load flow from Url: '{}'", url))
+            Err(e) => UIContext::ui_error(&format!("Could not load flow from Url: '{}'. {}", url, e.to_string()))
         }
     });
 }
@@ -82,8 +110,8 @@ pub fn open_flow(url: String) {
 /// with it.
 pub fn open_manifest(url: String) {
     std::thread::spawn(move || {
-        let provider = MetaProvider {};
-        match Manifest::load(&provider, &url) {
+        let provider = &MetaProvider::new(get_lib_search_path()) as &dyn Provider;
+        match Manifest::load(provider, &url) {
             Ok((manifest, _)) => {
                 match UICONTEXT.try_lock() {
                     Ok(mut context) => context.set_manifest(Some(url), Some(manifest)),
@@ -101,7 +129,7 @@ pub fn open_manifest(url: String) {
 /// This may result in output to stdout, stderr or other runtime functions that will be reflected on the UI.
 pub fn run_manifest(manifest_url: String, args: Vec<String>) {
     std::thread::spawn(move || {
-        match Coordinator::server(1, true /* native */, false, false, None) {
+        match Coordinator::server(1, get_lib_search_path(), true /* native */, false, false, None) {
             Ok(runtime_connection) => {
                 UIContext::clear_pre_run();
                 UIContext::message(&format!("Submitting manifest for execution with args: '{:?}'", args));
--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,28 @@
 //! The `flowide` is an IDE for `flow` programs.
-use std::{env, process};
-use std::sync::{Arc, Mutex};
-use log::error;
+use std::process;
+use log::{warn, error};
 
 use gio::prelude::*;
-use gtk::{Application, ApplicationWindow, Justification, MenuItem, ScrolledWindow, TextBuffer, Widget, WidgetExt, WindowPosition};
-use gtk::prelude::*;
-use gtk_rs_state::gtk_refs;
-use lazy_static::lazy_static;
+use gtk::Application;
+use build_ui::build_ui;
 
-// Modules
 mod ide_runtime_client;
 mod menu;
 mod ui_context;
 mod actions;
 mod options;
 mod build_ui;
+mod ui_layout;
+mod toolbar;
+mod notebook;
 
 // For logging errors related with the UI that suggest displaying them on the UI maybe impossible
 pub fn log_error(message: &str) {
     error!("UI message: {}", message);
+}
+
+pub fn log_warn(message: &str) {
+    warn!("UI message: {}", message);
 }
 
 fn main() {
